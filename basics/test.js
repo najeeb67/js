8af1c74100522bac0d8d7ca576ffc3a8d6ea62e8
najeeb67 <149230449+najeeb67@users.noreply.github.com>
2023-10-28 11:36:46 +0000

    confi

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,21 @@
-console.log("Najeeb")
+// const AccountId = 22145
+// let Email = "Najeeb@gmailcom"
+// var Number = "33"
+// AccountCity = "lahore"
+
+// // AccountId = 345
+// Email= "4542@gmail.com"
+// Number= "333333"
+// AccountCity = "multan"
+// console.table([AccountId, Email, Number, AccountCity])
+
+// console.log(typeof Lahore);
+// console.log(typeof 33)
+
+//sting
+
+let A = "hello"
+let b = " Najeeb"
+
+let c = A+b
+console.log(c)
